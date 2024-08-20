376f4bb9cbffc38687cef32dd7272d109e8780e8
kaikitano0610 <k.kitano.kai@gmail.com>
2024-08-20 13:36:07 +0900

    修正しました！

--- tests/Feature/TodoTest.php
+++ tests/Feature/TodoTest.php
@@ -29,21 +29,18 @@ class TodoTest extends TestCase
         $response->assertStatus(200);
 
 
-        //ログインしないと入れない状態である確認（
+        //ログインしないと入れない状態である確認
         // ホームページ
         $response = $this->get('/home');
-        $response->assertStatus(302); // リダイレクトされる
+        $response->assertStatus(302);
 
         // タスク作成ページ
         $response = $this->get('/home/task_create');
-        $response->assertStatus(302); // リダイレクトされる
+        $response->assertStatus(302);
 
         // コメント作成ページ
         $response = $this->get('/home/comment_create');
-        $response->assertStatus(302); // リダイレクトされる
-
-
-
+        $response->assertStatus(302);
     }
 
     //ユーザーを作成して、ログイン後のページを表示
@@ -87,7 +84,6 @@ class TodoTest extends TestCase
             'title' => '社会',
             'contents' => '地図帳で福岡県を確認',
             'due_date' => '2024-08-19',
-            'user_id' => $user->id
         ]);
         $response->assertStatus(302);
         $response->assertRedirect("/home");
@@ -103,7 +99,6 @@ class TodoTest extends TestCase
         //コメントの作成
         $response = $this->actingAs($user)->post('/comments', [
             '_token' => csrf_token(),
-            'user_id' => $user->id,
             'content' => '今日も一日頑張りました',
         ]);
 
@@ -137,7 +132,6 @@ class TodoTest extends TestCase
             'title' => '社会',
             'contents' => '地図帳で福岡県を確認',
             'due_date' => '2024-08-19',
-            'user_id' => $user->id
         ]);
         $response->assertStatus(302);
         $response->assertRedirect("/home");
@@ -150,7 +144,6 @@ class TodoTest extends TestCase
             'title' => '社会',
             'contents' => '地図帳で長崎県を確認',
             'due_date' => '2024-08-19',
-            'user_id' => $user->id
         ]);
 
         //変更したデータベースがあるか確認
@@ -164,7 +157,6 @@ class TodoTest extends TestCase
         //コメントの作成
         $response = $this->actingAs($user)->post('/comments', [
             '_token' => csrf_token(),
-            'user_id' => $user->id,
             'content' => '今日も一日頑張りました',
         ]);
 
@@ -172,7 +164,6 @@ class TodoTest extends TestCase
         $comment = Comment::where('user_id', $user->id)->first();
         $comment_id = $comment->id;
         $response = $this->actingAs($user)->put('/comments/' . $comment_id , [
-            'user_id' => $user->id,
             'content' => '明日も頑張りましょう',
         ]);
 
@@ -206,7 +197,6 @@ class TodoTest extends TestCase
             'title' => '英語',
             'contents' => '単語帳を覚えてくる',
             'due_date' => '2024-08-20',
-            'user_id' => $user->id
         ]);
         $response->assertStatus(302);
         $response->assertRedirect("/home");
